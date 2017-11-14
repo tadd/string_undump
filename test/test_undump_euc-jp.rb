3d19059c29656890432dc72ea5405b5904978c69
Tadashi Saito <tad.a.digger@gmail.com>
2017-11-14 10:20:23 +0900

    add test

--- test/test_undump_euc-jp.rb
+++ test/test_undump_euc-jp.rb
@@ -1,4 +1,5 @@
 # encoding: euc-jp
+# test for multibyte and non-Unicode string
 require 'test-unit'
 
 require_relative '../lib/string_undump'
@@ -6,5 +7,9 @@ require_relative '../lib/string_undump'
 class TestUndumpEucJp < Test::Unit::TestCase
   def test_undump_badly
     assert_equal('すごーい', '\xA4\xB9\xA4\xB4\xA1\xBC\xA4\xA4'.undump_badly)
+  end
+
+  def test_undump_roughly
+    assert_equal('すごーい', '\xA4\xB9\xA4\xB4\xA1\xBC\xA4\xA4'.undump_roughly)
   end
 end
